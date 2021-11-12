1edd4a47dbab0877bfe4c99f5dfb412866c2c5bc
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2021-11-12 11:16:27 -0500

    Better figure script names

--- figure3_training_data.jl
+++ figure3_training_data.jl
@@ -10,25 +10,6 @@ ids_test = setdiff(FreeConvection.SIMULATION_IDS, ids_train)
 data = load_data(ids_train, ids_test, Nz)
 datasets = data.coarse_datasets
 
-function color(id; alpha=1.0)
-    colors = CairoMakie.Makie.wong_colors(alpha)
-     1 <= id <= 9  && return colors[1]
-    10 <= id <= 12 && return colors[2]
-    13 <= id <= 15 && return colors[3]
-    16 <= id <= 18 && return colors[4]
-    19 <= id <= 21 && return colors[5]
-    error("Invalid ID: $id")
-end
-
-function label(id)
-     1 <= id <= 9  && return "training"
-    10 <= id <= 12 && return "Qb interpolation"
-    13 <= id <= 15 && return "Qb extrapolation"
-    16 <= id <= 18 && return "N² interpolation"
-    19 <= id <= 21 && return "N² extrapolation"
-    error("Invalid ID: $id")
-end
-
 fig = Figure()
 
 ax = fig[1, 1] = Axis(fig, xlabel="Temperature (°C)", ylabel="z (m)", title="6 hours")
@@ -39,7 +20,7 @@ for id in FreeConvection.SIMULATION_IDS
     zc = znodes(T)
     T_n = interior(T)[1, 1, :, 36] # 6 hours
     linestyle = 1 <= id <= 9 ? :solid : :dash
-    lines!(ax, T_n, zc, color=color(id); linestyle)
+    lines!(ax, T_n, zc, color=simulation_color(id); linestyle)
 end
 
 ylims!(-256, 0)
@@ -59,7 +40,7 @@ for id in FreeConvection.SIMULATION_IDS
     # wT_missing_n = interior(wT_missing)[1, 1, :, 36]
 
     linestyle = 1 <= id <= 9 ? :solid : :dash
-    lines!(ax, wT_n, zf, color=color(id), linestyle=linestyle)
+    lines!(ax, wT_n, zf, color=simulation_color(id), linestyle=linestyle)
 end
 
 ylims!(-256, 0)
@@ -72,7 +53,7 @@ for id in FreeConvection.SIMULATION_IDS
     zc = znodes(T)
     T_n = interior(T)[1, 1, :, 576] # 4 days
     linestyle = 1 <= id <= 9 ? :solid : :dash
-    lines!(ax, T_n, zc, color=color(id); linestyle)
+    lines!(ax, T_n, zc, color=simulation_color(id); linestyle)
 end
 
 ylims!(-256, 0)
@@ -92,15 +73,15 @@ for id in FreeConvection.SIMULATION_IDS
     # wT_missing_n = interior(wT_missing)[1, 1, :, 576]
 
     linestyle = 1 <= id <= 9 ? :solid : :dash
-    lines!(ax, wT_n, zf, color=color(id), linestyle=linestyle)
+    lines!(ax, wT_n, zf, color=simulation_color(id), linestyle=linestyle)
 end
 
 ylims!(-256, 0)
 
 entry_ids = (1, 10, 13, 16, 19)
-entries = [LineElement(color=color(id), linestyle = id == 1 ? :solid : :dash) for id in entry_ids]
-labels = [label(id) for id in entry_ids]
+entries = [LineElement(color=simulation_color(id), linestyle = id == 1 ? :solid : :dash) for id in entry_ids]
+labels = [simulation_label(id) for id in entry_ids]
 Legend(fig[0, :], entries, labels, framevisible=false, orientation=:horizontal, tellwidth=false, tellheight=true)
 
-save("figure_training_data.png", fig, px_per_unit=2)
-save("figure_training_data.pdf", fig, pt_per_unit=2)
+save("figure3_training_data.png", fig, px_per_unit=2)
+save("figure3_training_data.pdf", fig, pt_per_unit=2)
