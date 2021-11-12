160435c9d0d74aa1624b26738f2286adc14764dd
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2021-11-12 16:53:09 -0500

    Better figure 3

--- figure3_training_data.jl
+++ figure3_training_data.jl
@@ -2,85 +2,70 @@ using CairoMakie
 using Oceananigans
 using FreeConvection
 
+using CairoMakie.Makie: wong_colors
+
 Nz = 32
 
-ids_train = collect(1:9)
-ids_test = setdiff(FreeConvection.SIMULATION_IDS, ids_train)
+# data = load_data(ids_train, ids_test, Nz)
+# datasets = data.coarse_datasets
 
-data = load_data(ids_train, ids_test, Nz)
-datasets = data.coarse_datasets
+id = 3  # Simulation to plot
+
+n0  = 1   # time index for t = 0
+n6  = 73  # time index for t = 12 hours
+n96 = 577 # time index for t = 4 days
+
+ns = [n0, n6, n96]
+
+ds = datasets[id]
+
+K = 0.2
+add_convective_adjustment_flux!(ds, K)
+
+T = ds["T"]
+wT = ds["wT"]
+wT_param = ds["wT_param"]
+wT_missing = ds["wT_missing"]
+
+zc = znodes(T)
+zf = znodes(wT)
+
+colors = wong_colors()
 
 fig = Figure()
 
-ax = fig[1, 1] = Axis(fig, xlabel="Temperature (°C)", ylabel="z (m)", title="6 hours")
+ax = fig[1, 1] = Axis(fig, xlabel="Temperature (°C)", ylabel="z (m)")
 
-for id in FreeConvection.SIMULATION_IDS
-    ds = datasets[id]
-    T = ds["T"]
-    zc = znodes(T)
-    T_n = interior(T)[1, 1, :, 36] # 6 hours
-    linestyle = 1 <= id <= 9 ? :solid : :dash
-    lines!(ax, T_n, zc, color=simulation_color(id); linestyle)
+for (i, n) in enumerate(ns)
+    T_n = interior(T)[1, 1, :, n]
+    lines!(ax, T_n, zc, linewidth=3, color=colors[i])
 end
 
-ylims!(-256, 0)
+ax.yticks = 0:-32:-128
 
-ax = fig[2, 1] = Axis(fig, xlabel="Heat flux (m/s K)", ylabel="z (m)")
+xlims!(19.75, 20)
+ylims!(-128, 0)
 
-for id in FreeConvection.SIMULATION_IDS
-    ds = datasets[id]
-    wT = ds["wT"]
-    # wT_param = ds["wT_param"]
-    # wT_missing = ds["wT_missing"]
-    zf = znodes(wT)
+ax = fig[1, 2] = Axis(fig, xlabel="Heat flux (m/s K)")
 
-    # 6 hours
-    wT_n = interior(wT)[1, 1, :, 36]
-    # wT_param_n = interior(wT_param)[1, 1, :, 36]
-    # wT_missing_n = interior(wT_missing)[1, 1, :, 36]
+for (i, n) in enumerate(ns)
+    wT_n = interior(wT)[1, 1, :, n]
+    lines!(ax, wT_n, zf, linewidth=3, color=colors[i])
 
-    linestyle = 1 <= id <= 9 ? :solid : :dash
-    lines!(ax, wT_n, zf, color=simulation_color(id), linestyle=linestyle)
+    wT_param_n = interior(wT_param)[1, 1, :, n]
+    lines!(ax, wT_param_n, zf, linewidth=3, color=colors[i], linestyle=:dash)
+
+    wT_missing_n = interior(wT_missing)[1, 1, :, n]
+    lines!(ax, wT_missing_n, zf, linewidth=3, color=colors[i], linestyle=:dot)
 end
 
-ylims!(-256, 0)
+ax.yticks = 0:-32:-128
+ax.ytickformat = ys -> ["" for y in ys]
 
-ax = fig[1, 2] = Axis(fig, xlabel="Temperature (°C)", title="4 days")
+ylims!(-128, 0)
 
-for id in FreeConvection.SIMULATION_IDS
-    ds = datasets[id]
-    T = ds["T"]
-    zc = znodes(T)
-    T_n = interior(T)[1, 1, :, 576] # 4 days
-    linestyle = 1 <= id <= 9 ? :solid : :dash
-    lines!(ax, T_n, zc, color=simulation_color(id); linestyle)
-end
-
-ylims!(-256, 0)
-
-ax = fig[2, 2] = Axis(fig, xlabel="Heat flux (m/s K)", ylabel="z (m)")
-
-for id in FreeConvection.SIMULATION_IDS
-    ds = datasets[id]
-    wT = ds["wT"]
-    # wT_param = ds["wT_param"]
-    # wT_missing = ds["wT_missing"]
-    zf = znodes(wT)
-
-    # 4 days
-    wT_n = interior(wT)[1, 1, :, 576]
-    # wT_param_n = interior(wT_param)[1, 1, :, 576]
-    # wT_missing_n = interior(wT_missing)[1, 1, :, 576]
-
-    linestyle = 1 <= id <= 9 ? :solid : :dash
-    lines!(ax, wT_n, zf, color=simulation_color(id), linestyle=linestyle)
-end
-
-ylims!(-256, 0)
-
-entry_ids = (1, 10, 13, 16, 19)
-entries = [LineElement(color=simulation_color(id), linestyle = id == 1 ? :solid : :dash) for id in entry_ids]
-labels = [simulation_label(id) for id in entry_ids]
+entries = [LineElement(color=colors[l]) for l in 1:3]
+labels = ["t = 0", "t = 12 hours", "t = 4 days"]
 Legend(fig[0, :], entries, labels, framevisible=false, orientation=:horizontal, tellwidth=false, tellheight=true)
 
 save("figure3_training_data.png", fig, px_per_unit=2)
