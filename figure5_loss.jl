e23ef2ad45c1c75f4e87994f2d5b163e710c7651
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2021-11-15 16:13:55 -0500

    Option to normalize figure 5

--- figure5_loss.jl
+++ figure5_loss.jl
@@ -6,7 +6,7 @@ using FreeConvection
 
 using Flux.Losses: mse
 
-function plot_epoch_loss_summary_filled_curves(ids, nde_solutions, true_solutions, T_scaling; filepath_prefix, alpha=0.3)
+function plot_epoch_loss_summary_filled_curves(ids, nde_solutions, true_solutions, T_scaling; filepath_prefix, normalize=false, alpha=0.3)
     epochs = length(nde_solutions[first(ids)])
 
     fig = Figure()
@@ -20,9 +20,16 @@ function plot_epoch_loss_summary_filled_curves(ids, nde_solutions, true_solution
     for sub_ids in (1:9, 10:12, 13:15, 16:18, 19:21)
         min_loss_training = [minimum([loss_histories[id][e] for id in sub_ids]) for e in 1:epochs]
         max_loss_training = [maximum([loss_histories[id][e] for id in sub_ids]) for e in 1:epochs]
-        band!(ax1, 1:epochs, min_loss_training, max_loss_training, color=simulation_color(sub_ids[1]; alpha))
-
         mean_loss_training = [mean([loss_histories[id][e] for id in sub_ids]) for e in 1:epochs]
+
+        if normalize
+            normalization_factor = maximum(max_loss_training)
+            min_loss_training ./= normalization_factor
+            max_loss_training ./= normalization_factor
+            mean_loss_training ./= normalization_factor
+        end
+
+        band!(ax1, 1:epochs, min_loss_training, max_loss_training, color=simulation_color(sub_ids[1]; alpha))
         lines!(ax1, 1:epochs, mean_loss_training, color=simulation_color(sub_ids[1]))
     end
 
@@ -49,5 +56,6 @@ T_scaling = file["T_scaling"]
 ids = keys(nde_solutions) |> collect |> sort
 
 plot_epoch_loss_summary_filled_curves(ids, nde_solutions, true_solutions, T_scaling, filepath_prefix="figure5_loss")
+plot_epoch_loss_summary_filled_curves(ids, nde_solutions, true_solutions, T_scaling, filepath_prefix="figure5_loss_normalized", normalize=true)
 
 close(file)
