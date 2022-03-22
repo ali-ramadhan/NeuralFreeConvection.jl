6d2f178d0b56c299a93c2016451c82f68aac7672
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2022-03-21 20:03:22 -0400

    More beautiful figure 6

--- figure6_loss_differential_integral_control.jl
+++ figure6_loss_differential_integral_control.jl
@@ -1,64 +1,83 @@
 using Statistics
-
+using ArgParse
 using JLD2
 using CairoMakie
 using FreeConvection
 
 using Flux.Losses: mse
 using CairoMakie.Makie: wong_colors
-
 using Oceananigans: interior
 using Oceananigans.Units: days
 
-function figure6_initial_vs_final_loss_matrix_filled_curves(datasets, nde_sols, initial_nde_sols, T_scaling; filepath_prefix, alpha=0.3, ylims=(1e-6, 1e-1))
+function parse_command_line_arguments()
+    settings = ArgParseSettings()
 
-    loss(T, T̂) = mse(T_scaling.(T), T_scaling.(T̂))
+    @add_arg_table! settings begin
+        "--output-directory-fluxes"
+            help = "Output directory for neural network trained on fluxes."
+            arg_type = String
 
-    T = datasets[1]["T"]
-    Nt = size(T, 4)
-    times = T.times ./ days
-
-    T_solution = Dict(id => [interior(ds["T"])[1, 1, :, n] for n in 1:Nt] for (id, ds) in datasets)
-    loss_nde = Dict(id => [loss(T_solution[id][n], nde_sols[id].T[:, n]) for n in 1:Nt] for id in keys(datasets))
-    loss_init = Dict(id => [loss(T_solution[id][n], initial_nde_sols[id].T[:, n]) for n in 1:Nt] for id in keys(datasets))
-
-    loss_nde_min = [minimum([loss_nde[id][n] for id in keys(datasets)]) for n in 1:Nt]
-    loss_nde_max = [maximum([loss_nde[id][n] for id in keys(datasets)]) for n in 1:Nt]
-    loss_nde_mean = [mean([loss_nde[id][n] for id in keys(datasets)]) for n in 1:Nt]
-
-    loss_init_min = [minimum([loss_init[id][n] for id in keys(datasets)]) for n in 1:Nt]
-    loss_init_max = [maximum([loss_init[id][n] for id in keys(datasets)]) for n in 1:Nt]
-    loss_init_mean = [mean([loss_init[id][n] for id in keys(datasets)]) for n in 1:Nt]
-
-    for loss_param_stat in (loss_nde_min, loss_nde_max, loss_nde_mean, loss_init_min, loss_init_max, loss_init_mean)
-        replace!(x -> iszero(x) ? NaN : x, loss_param_stat)
+        "--output-directory-timeseries"
+            help = "Output directory for neural differential equation trained on time series."
+            arg_type = String
     end
+
+    return parse_args(settings)
+end
+
+function figure6_differential_vs_integral_loss(ids, times, solution_loss_history_tof, solution_loss_history_tots; filepath_prefix, rows=2, cols=3, alpha=0.3)
+    _, Nt = size(solution_loss_history_tof[first(ids)])
+
+    remove_zeros!(xs) = replace!(x -> iszero(x) ? NaN : x, xs)
 
     fig = Figure()
 
     colors = wong_colors()
     colors_alpha = wong_colors(alpha)
 
-    ax = fig[1, 1] = Axis(fig, xlabel="Simulation time (days)", ylabel="Loss", yscale=log10)
+    # Ordered so that training subplot shows up at the bottom.
+    simulation_ids = (10:12, 16:18, 1:9, 13:15, 19:21)
 
-    band!(ax, times, loss_nde_min, loss_nde_max, color=colors_alpha[1])
-    lines!(ax, times, loss_nde_mean, color=colors[1])
+    for (N, sub_ids) in enumerate(simulation_ids)
+        i = mod(N-1, cols) + 1
+        j = div(N-1, cols) + 1
 
-    band!(ax, times, loss_init_min, loss_init_max, color=colors_alpha[2])
-    lines!(ax, times, loss_init_mean, color=colors[2])
+        xlabel = (i, j) == (3, 1) ? "Simulation time (days)" : ""
+        ylabel = (i, j) == (2, 1) ? "Loss" : ""
+        ax = Axis(fig[i, j], title=simulation_label(sub_ids[1]), xlabel=xlabel, ylabel=ylabel, yscale=log10,
+                             xgridvisible=false, ygridvisible=false, xticklabelsvisible=i == cols, yticklabelsvisible=j == 1)
 
-    CairoMakie.xlims!(0, times[end])
-    CairoMakie.ylims!(ylims...)
+        loss_nde_tof_min = [minimum([solution_loss_history_tof[id][end, n] for id in sub_ids]) for n in 1:Nt] |> remove_zeros!
+        loss_nde_tof_max = [maximum([solution_loss_history_tof[id][end, n] for id in sub_ids]) for n in 1:Nt] |> remove_zeros!
+        loss_nde_tof_mean = [mean([solution_loss_history_tof[id][end, n] for id in sub_ids]) for n in 1:Nt] |> remove_zeros!
 
-    entries = [PolyElement(color=c) for c in colors[1:2]]
-    labels = ["Trained on time series", "Trained on fluxes"]
-    Legend(fig[0, 1], entries, labels, orientation=:horizontal, tellheight=true, framevisible=false)
+        loss_nde_tots_min = [minimum([solution_loss_history_tots[id][end, n] for id in sub_ids]) for n in 1:Nt] |> remove_zeros!
+        loss_nde_tots_max = [maximum([solution_loss_history_tots[id][end, n] for id in sub_ids]) for n in 1:Nt] |> remove_zeros!
+        loss_nde_tots_mean = [mean([solution_loss_history_tots[id][end, n] for id in sub_ids]) for n in 1:Nt] |> remove_zeros!
+
+        band!(ax, times, loss_nde_tof_min, loss_nde_tof_max, color=colors_alpha[2])
+        lines!(ax, times, loss_nde_tof_mean, color=colors[2])
+
+        band!(ax, times, loss_nde_tots_min, loss_nde_tots_max, color=colors_alpha[3])
+        lines!(ax, times, loss_nde_tots_mean, color=colors[3])
+
+        xlims!(ax, (0, times[end]))
+        ylims!(ax, (1e-6, 1e-2))
+    end
+
+    entries = [PolyElement(color=c) for c in colors[2:3]]
+    labels = ["Trained on fluxes", "Trained on time series"]
+    Legend(fig[3, 2], entries, labels, framevisible=false, tellwidth=false, tellheight=false)
 
     save(filepath_prefix * ".png", fig, px_per_unit=2)
     save(filepath_prefix * ".pdf", fig, pt_per_unit=2)
 
-    return nothing
+    return fig
 end
+
+args = parse_command_line_arguments()
+output_dir_tof = args["output-directory-fluxes"]
+output_dir_tots = args["output-directory-timeseries"]
 
 Nz = 32
 
@@ -66,20 +85,17 @@ ids_train = 1:9
 ids_test = setdiff(FreeConvection.SIMULATION_IDS, ids_train)
 
 data = load_data(ids_train, ids_test, Nz)
-datasets = data.coarse_datasets
 
-# Temporary until simulations 19-21 are regenerated.
-delete!(datasets, 19)
-delete!(datasets, 20)
-delete!(datasets, 21)
+history_filepath_tof = joinpath(output_dir_tof, "neural_network_history_trained_on_fluxes.jld2")
+history_filepath_tots = joinpath(output_dir_tots, "neural_network_history_trained_on_timeseries.jld2")
 
-file = jldopen("solutions_and_history.jld2", "r")
+file_tof = jldopen(history_filepath_tof)
+file_tots = jldopen(history_filepath_tots)
 
-nde_solutions = file["nde"]
-initial_nde_solutions = file["initial_nde"]
-T_scaling = file["T_scaling"]
+solution_loss_history_tof = file_tof["solution_loss_history"]
+solution_loss_history_tots = file_tots["solution_loss_history"]
 
-filepath_prefix = "figure6_online_vs_offline_loss"
-figure6_initial_vs_final_loss_matrix_filled_curves(datasets, nde_solutions, initial_nde_solutions, T_scaling; filepath_prefix)
-
-close(file)
+ids = keys(data.coarse_datasets) |> collect |> sort
+times = data.coarse_datasets[1]["T"].times ./ days
+filepath_prefix = "figure6_differential_vs_integral_loss"
+figure6_differential_vs_integral_loss(ids, times, solution_loss_history_tof, solution_loss_history_tots; filepath_prefix)
